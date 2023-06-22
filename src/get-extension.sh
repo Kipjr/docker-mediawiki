70a2a6c971fc34d2e22ad321da60ffcdff8e35fb
Martijn <12066560+Kipjr@users.noreply.github.com>
2023-06-22 22:35:40 +0200

    Update get-extension.sh

--- src/get-extension.sh
+++ src/get-extension.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 EXT=$1
-VER=${2:-REL1_39}
+VER=${2:-1.39}
 
-
+VER_STRING="REL${VER/./_}"
 #curl --no-progress-meter -G --connect-timeout 5  -H "Connection: close"  -o ${EXT}.html https://www.mediawiki.org/wiki/Special:ExtensionDistributor?extdistname=${EXT}&extdistversion=REL${VER}
 
-curl "https://www.mediawiki.org/wiki/Special:ExtensionDistributor?extdistname=${EXT}&extdistversion=${VER}" --compressed -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-GB,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: cross-site' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: trailers' -o "/tmp/${EXT}.html"
+curl "https://www.mediawiki.org/wiki/Special:ExtensionDistributor?extdistname=${EXT}&extdistversion=${VER_STRING}" --compressed -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' -H 'Accept-Language: en-GB,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: cross-site' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: trailers' -o "/tmp/${EXT}.html"
 
 sleep 1
 
